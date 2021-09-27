ab975e11bd246fbf1ff718c09dab56e5ee3b2075
Steve Briley <24689390+SteveBriley@users.noreply.github.com>
2021-09-26 23:08:27 -0400

    go fmt

--- src/config/config.go
+++ src/config/config.go
@@ -1,95 +1,95 @@
 package config
 
 import (
-    "flag"
-    "os"
-    "path/filepath"
-    "k8s.io/client-go/kubernetes"
-    "k8s.io/client-go/rest"
-    "k8s.io/client-go/tools/clientcmd"
-    "k8s.io/client-go/util/homedir"
+	"flag"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"os"
+	"path/filepath"
 )
 
 type Config struct {
-    kconf       *rest.Config
-    clientset   kubernetes.Interface
-    global      Global
+	kconf     *rest.Config
+	clientset kubernetes.Interface
+	global    Global
 }
 
 type Global struct {
-    killTime    string
-    vendor      string
-    loopSeconds string
-    logLevel    string
+	killTime    string
+	vendor      string
+	loopSeconds string
+	logLevel    string
 }
 
 func NewConfig() *Config {
-    cfg := &Config {}
-    cfg.Populate()
-    return cfg
+	cfg := &Config{}
+	cfg.Populate()
+	return cfg
 }
 
 func getEnv(key, fallback string) string {
-    if value, ok := os.LookupEnv(key); ok {
-        return value
-    }
-    return fallback
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
 
 func (c *Config) GetClientset() kubernetes.Interface {
-    return c.clientset
+	return c.clientset
 }
 func (c *Config) GetLogLevel() string {
-    return c.global.logLevel
+	return c.global.logLevel
 }
 func (c *Config) GetVendor() string {
-    return c.global.vendor
+	return c.global.vendor
 }
 func (c *Config) GetKillTime() string {
-    return c.global.killTime
+	return c.global.killTime
 }
 func (c *Config) GetLoopSeconds() string {
-    return c.global.loopSeconds
+	return c.global.loopSeconds
 }
 
 func (c *Config) Populate() {
-    //var err error
-    kconf, clientset, err := k8sConfig()
-    if err == nil {
-        c.kconf = kconf
-        c.clientset = clientset
-    }
-    c.global.killTime = getEnv("killTime", "48h")
-    c.global.vendor = getEnv("vendor", "reaper.io")
-    c.global.loopSeconds = getEnv("loopSeconds", "10")
-    c.global.logLevel = getEnv("logLevel", "trace")
+	//var err error
+	kconf, clientset, err := k8sConfig()
+	if err == nil {
+		c.kconf = kconf
+		c.clientset = clientset
+	}
+	c.global.killTime = getEnv("killTime", "48h")
+	c.global.vendor = getEnv("vendor", "reaper.io")
+	c.global.loopSeconds = getEnv("loopSeconds", "10")
+	c.global.logLevel = getEnv("logLevel", "trace")
 }
 
 func k8sConfig() (*rest.Config, kubernetes.Interface, error) {
-    var config *rest.Config
-    if _, err := os.Stat("/var/run/secrets/kubernetes.io/serviceaccount"); err == nil {
-        config, err = rest.InClusterConfig()
-        if err != nil {
-            panic(err.Error())
-        }
-    } else {
-        var kubeconfig *string
-        if home := homedir.HomeDir(); home != "" {
-            kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-        } else {
-            kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-        }
-        flag.Parse()
+	var config *rest.Config
+	if _, err := os.Stat("/var/run/secrets/kubernetes.io/serviceaccount"); err == nil {
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
+	} else {
+		var kubeconfig *string
+		if home := homedir.HomeDir(); home != "" {
+			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		} else {
+			kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+		}
+		flag.Parse()
 
-        config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
-        if err != nil {
-            panic(err.Error())
-        }
-    }
-    clientset, err := kubernetes.NewForConfig(config)
-    if err != nil {
-        panic(err.Error())
-    }
+		config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
 
-    return config, clientset, nil
+	return config, clientset, nil
 }
--- src/main.go
+++ src/main.go
@@ -2,18 +2,17 @@ package main
 
 //imports
 import (
-    "os"
-    "os/signal"
-    "github.com/jcatana/reaper/config"
-    "github.com/jcatana/reaper/logging"
-    "github.com/jcatana/reaper/watcher"
-    "github.com/jcatana/reaper/reaper"
-    "github.com/sirupsen/logrus"
+	"github.com/jcatana/reaper/config"
+	"github.com/jcatana/reaper/logging"
+	"github.com/jcatana/reaper/reaper"
+	"github.com/jcatana/reaper/watcher"
+	"github.com/sirupsen/logrus"
+	"os"
+	"os/signal"
 
-    "k8s.io/client-go/informers"
-    corev1 "k8s.io/api/core/v1"
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/informers"
 )
 
 //global variables
@@ -22,26 +21,26 @@ var log *logrus.Logger
 
 //init: get the configuration, set up logging
 func init() {
-    cfg = config.NewConfig()
-    log = logging.NewLogger(cfg)
+	cfg = config.NewConfig()
+	log = logging.NewLogger(cfg)
 }
 
 //main
-func main () {
-    reap := watcher.NewWatcher()
+func main() {
+	reap := watcher.NewWatcher()
 
-    factory := informers.NewFilteredSharedInformerFactory(cfg.GetClientset(), 0, corev1.NamespaceAll, func(options *metav1.ListOptions) {options.LabelSelector = cfg.GetVendor() + "/enabled=True"})
-    informer := factory.Core().V1().Namespaces()
+	factory := informers.NewFilteredSharedInformerFactory(cfg.GetClientset(), 0, corev1.NamespaceAll, func(options *metav1.ListOptions) { options.LabelSelector = cfg.GetVendor() + "/enabled=True" })
+	informer := factory.Core().V1().Namespaces()
 
-    stopper := make(chan struct{})
+	stopper := make(chan struct{})
 
-    //launch go routines
-    go watcher.StartWatching(stopper, informer.Informer(), log, cfg, reap) //launch watcher
-    go reaper.Reap(stopper, log, cfg, reap) //launch reaper
+	//launch go routines
+	go watcher.StartWatching(stopper, informer.Informer(), log, cfg, reap) //launch watcher
+	go reaper.Reap(stopper, log, cfg, reap)                                //launch reaper
 
-    signalChannel := make(chan os.Signal, 0)
-    signal.Notify(signalChannel, os.Kill, os.Interrupt)
+	signalChannel := make(chan os.Signal, 0)
+	signal.Notify(signalChannel, os.Kill, os.Interrupt)
 
-    <-signalChannel
-    close(stopper)
+	<-signalChannel
+	close(stopper)
 }
